92d0dd47c2aab3034148d93f21852ca8a7d07f0d
aderitorafael16 <aderitobento16@gmail.com>
2024-03-10 20:07:48 +0100

    wip: create structure, page index and others components

---
+++ src/app.tsx
@@ -0,0 +1,75 @@
+import { ChangeEvent, useState } from "react";
+import { NewNoteCard } from "./components/new-note-card";
+import { NoteCard } from "./components/note-card";
+
+import logo from './assets/Logo.svg'
+
+interface Note {
+  id: string,
+  date: Date,
+  content: string
+}
+
+export function App() {
+
+  const [search, setSearch] = useState('')
+
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notes')
+
+    if(notesOnStorage) {
+      return JSON.parse(notesOnStorage)
+    }
+
+    return []
+  })
+  
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content
+    }
+
+    const notesArray = [newNote, ...notes]
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  function handleSeacrh(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  const filteredNotes = search !== '' 
+    ? notes.filter(note => note.content.toLocaleUpperCase().includes(search.toLocaleUpperCase()))
+    : notes
+
+  return (
+    <div className="min-h-screen mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="NLW Expert" />
+
+      <form className="w-full">
+        <input 
+          type="text" 
+          placeholder="Busque em suas notas..." 
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          onChange={handleSeacrh}
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+
+        {filteredNotes.map((note) => (
+          <NoteCard key={note.id} note={{date: note.date, content: note.content}} />
+        ))}
+      </div>
+    </div>
+  )
+}
